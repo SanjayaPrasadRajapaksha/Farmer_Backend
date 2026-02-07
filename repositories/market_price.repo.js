eca8629f6ebf62de7cfbed49ade9fddeab6f15fe
SanjayaPrasadRajapaksha <sanjayaprasad823@gmail.com>
2026-02-08 00:51:02 +0530

    test: add Supertest file for market price API

--- repositories/market_price.repo.js
+++ repositories/market_price.repo.js
@@ -66,50 +66,6 @@ const MarketPriceRepository = {
     return Market_Price.bulkCreate(rows)
   },
 
-  // Update existing rows by composite key (product/location/type/date), otherwise insert.
-  // This prevents wiping all existing market prices when uploading another PDF for the same day.
-  // async upsertMarketPricesByCompositeKey(rows) {
-  //   if (!Array.isArray(rows) || rows.length === 0) return 0
-
-  //   const sequelize = Market_Price.sequelize
-  //   return sequelize.transaction(async (transaction) => {
-  //     let affected = 0
-
-  //     for (const row of rows) {
-  //       const {
-  //         product_id,
-  //         economic_center_location_id,
-  //         price_type_id,
-  //         Date: date,
-  //       } = row
-
-  //       const [updatedCount] = await Market_Price.update(
-  //         {
-  //           price: row.price,
-  //           isVerify: row.isVerify ?? false,
-  //         },
-  //         {
-  //           where: {
-  //             product_id,
-  //             economic_center_location_id,
-  //             price_type_id,
-  //             Date: date,
-  //           },
-  //           transaction,
-  //         }
-  //       )
-
-  //       if (updatedCount === 0) {
-  //         await Market_Price.create(row, { transaction })
-  //       }
-
-  //       affected += 1
-  //     }
-
-  //     return affected
-  //   })
-  // },
-
   async replaceMarketPricesForCriteria(criteria, data) {
     const sequelize = Market_Price.sequelize
     return sequelize.transaction(async (transaction) => {
