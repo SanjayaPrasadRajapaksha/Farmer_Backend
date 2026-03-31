5373efed77f479dd9320fac7763312a5c5b7d988
SanjayaPrasadRajapaksha <sanjayaprasad823@gmail.com>
2026-03-31 15:23:04 +0530

    fix: remove duplicate product routes

--- routes/product.route.js
+++ routes/product.route.js
@@ -6,7 +6,6 @@ const router = express.Router();
 router.post("/create", ProductController.create);
 router.put("/updateById/:id", ProductController.updateById);
 router.put("/uploadImage/:id", upload.single("image"), ProductController.uploadImage);
-router.post("/uploadImage/:id", upload.single("image"), ProductController.uploadImage);
 router.get("/getAll", ProductController.getAll);
 router.get("/getById/:id", ProductController.findById);
 router.delete("/deleteById/:id", ProductController.deleteById);
